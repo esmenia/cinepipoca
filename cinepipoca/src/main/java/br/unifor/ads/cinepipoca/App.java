0dc419677992c83a1837ebd7d7ef93f42a152f5c
Anderson Mendes <andersonmpm@gmail.com>
2014-09-19 19:58:30 -0300

    [Esmenia-Borba] carga inicial

---
+++ cinepipoca/src/main/java/br/unifor/ads/cinepipoca/App.java
@@ -0,0 +1,13 @@
+package br.unifor.ads.cinepipoca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
